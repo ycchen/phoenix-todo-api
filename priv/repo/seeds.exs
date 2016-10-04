bb89de64d7e3434edea75363b4a1e45b742ec344
Yungchih Chen <yungchih_chen@yahoo.com>
2016-10-04 16:58:04 -0400

    Added initial data using priv/repo/seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias TodosApi.Repo
+alias TodosApi.Todo
+
+Repo.insert!(%Todo{title: "Create the Phoenix App", is_completed: true})
+Repo.insert!(%Todo{title: "Prepare the Ember App", is_completed: false})
+Repo.insert!(%Todo{title: "Ensure the Apps Work Together", is_completed: false})
